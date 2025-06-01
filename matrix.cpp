3faa725b08f8d3272b22c3d9aaa1bf186cf4eb3b
Juan Jose Mayorga <jmayorgab@unal.edu.co>
2025-06-01 15:11:36 -0500

    name changed

---
+++ matrix.cpp
@@ -0,0 +1,41 @@
+#include "matrix.h"
+
+//Generate the matrix
+std::vector<int> generatematrix(int L, double p) {
+
+    int N = L * L;
+    std::vector<int> matrix(N, 0);  // Vector 1D contiguo
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(0.0, 1.0);
+
+    for (int i = 0; i < L; ++i) {
+
+        for (int j = 0; j < L; ++j) {
+
+            int id = i * L + j;  // Map 2D → 1D
+            double r = dis(gen);
+            matrix[id] = (r < p) ? 1 : 0; //Fills the matrix
+
+        }
+    }
+
+    return matrix;
+}
+
+
+//Print the percolation matrix
+void printmatrix(const std::vector<int>& matrix, int L) { 
+
+    for (int i = 0; i < L; ++i) {
+        for (int j = 0; j < L; ++j) {
+
+            int id = i * L + j;  // Map 2D → 1D
+            std::cout << (matrix[id] ? "1" : "0") << " ";
+
+        }
+
+        std::cout << '\n';
+    }
+}
