2ea55434c75d41d69abcfcebf545a08167bea5b3
Juan Jose Mayorga <jmayorgab@unal.edu.co>
2025-06-04 23:53:41 -0500

    se añadio lo que debe ejecutar la función main

--- matriz.sh
+++ matriz.sh
@@ -14,11 +14,11 @@ python clustervisualize.py
 #FUNCION MAIN
 #Obtenemos los valores de probabilidad como
 g++ -std=c++17 -o print.exe print.cpp probvalues.cpp
-.\print.exe 30 > prob.txt 
+./print.exe 30 > probabilidades.txt   
 
 #Sacamos probabilidades, tamaños etc
 g++ -std=c++17 -o clusterprob.exe clusterprob.cpp matrix.cpp dfspercolation.cpp probvalues.cpp hoshen_kopelman.cpp union_find.cpp
-parallel './clusterprob.exe {1} 20 {2} >> data-{1}.txt' ::: 8 16 32 64 128 256 512 ::: $(cat probabilidades.txt)
+parallel './clusterprob.exe {1} 50 {2} >> data-{1}.txt' ::: 32 64 128 256 512 ::: $(cat probabilidades.txt)
 
 #En caso de que la salida de los datos no se de ordenadamente, ordenamos los datos
 for level in 8 16 32 64 128 256 512; do
@@ -30,5 +30,5 @@ for level in 8 16 32 64 128 256 512; do
     fi
 done
 
-#graficamos probabilidad
+#graficamos probabilidad y tamaños
 python plotclusterprob.py
