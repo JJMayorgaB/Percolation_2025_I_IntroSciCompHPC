6470b052ea0bdf5dfad5a67be2a12a8d11e84048
Juan Jose <jmayorgab@unal.edu.co>
2025-06-06 11:13:17 -0500

    nothing

--- main.sh
+++ main.sh
@@ -1,11 +1,11 @@
 #FUNCION MAIN
 #Obtenemos los valores de probabilidad como
 g++ -std=c++17 -o print.exe print.cpp probvalues.cpp
-./print.exe 30 > probabilidades.txt   
+./print.exe 100 > probabilidades.txt   
 
 #Sacamos probabilidades, tamaños etc
-g++ -std=c++17 -o clusterprob.exe clusterprob.cpp matrix.cpp dfspercolation.cpp probvalues.cpp hoshen_kopelman.cpp union_find.cpp
-parallel './clusterprob.exe {1} 50 {2} >> data-{1}.txt' ::: 32 64 128 256 512 ::: $(cat probabilidades.txt)
+g++ -std=c++17 -o main.exe main.cpp matrix.cpp dfspercolation.cpp probvalues.cpp hoshen_kopelman.cpp union_find.cpp
+parallel './main.exe {1} 30 {2} >> data-{1}.txt' ::: 32 64 128 256 512 ::: $(cat probabilidades.txt)
 
 #En caso de que la salida de los datos no se de ordenadamente, ordenamos los datos
 for level in 32 64 128 256 512; do
