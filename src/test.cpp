ae06ce83c9b174bfc94919ccd6180fe05006c705
Juan Jose Mayorga <jmayorgab@unal.edu.co>
2025-06-11 00:15:32 -0500

    test.cpp ordenado

--- src/test.cpp
+++ src/test.cpp
@@ -3,30 +3,19 @@
 #include "../include/hoshen_kopelman.h"
 #include "../include/matrix.h"
 
-
-TEST_CASE("En arreglo de solo ceros nunca hay percolación, Y en arreglo de unos, siempre la hay.", "[unosceros]"){
+TEST_CASE("En arreglo de solo ceros nunca hay percolación", "[ceros]"){
 	
 	int n_muestras = 10;
-	std::vector<int> tamanhos = {10, 20, 30, 40, 50, 500, 1000, 5000, 10000};
+	std::vector<int> tamanhos = {10, 20, 30, 40, 50, 500, 1000, 5000};
 
 	for (int L: tamanhos){
 
-		std::vector<int> unos(L*L,1);
 		std::vector<int> ceros(L*L,0);
-
-
-		int percolating_count_unos = 0;
-		int total_cluster_size_unos = 0;
 
 		int percolating_count_ceros = 0;
 		int total_cluster_size_ceros = 0;
 
 		for (int ii = 0; ii < n_muestras; ii++){
-			ClusterInfo hk_unos = hoshen_kopelman(unos, L);
-			if (hk_unos.percolates){
-				percolating_count_unos += 1;
-				total_cluster_size_unos += hk_unos.max_cluster_size;
-			}
 			ClusterInfo hk_ceros = hoshen_kopelman(ceros, L);
 			if (hk_ceros.percolates){
 				// No se debería ejecutar
@@ -34,20 +23,42 @@ TEST_CASE("En arreglo de solo ceros nunca hay percolación, Y en arreglo de unos
 				total_cluster_size_ceros += hk_ceros.max_cluster_size;
 			}
 		}
-		double probability_unos = static_cast<double>(percolating_count_unos) / n_muestras;
 		double probability_ceros = static_cast<double>(percolating_count_ceros) / n_muestras;
-
-		double mean_cluster_size_unos = static_cast<double>(total_cluster_size_unos) / (n_muestras*(L*L));
 		double mean_cluster_size_ceros = static_cast<double>(total_cluster_size_ceros) / (n_muestras*(L*L));
 
-		REQUIRE(probability_unos == 1.0);
-		REQUIRE(mean_cluster_size_unos==1.0);
 		REQUIRE(probability_ceros == 0.0);
 		REQUIRE(mean_cluster_size_ceros==0);
 	}
 }
 
-TEST_CASE("Se detectan percolaciones verticales y horizontales","[horizontal_vertical]"){
+TEST_CASE("En arreglo de unos siempre hay percolación", "[unos]"){
+	
+	int n_muestras = 10;
+	std::vector<int> tamanhos = {10, 20, 30, 40, 50, 500, 1000, 5000};
+
+	for (int L: tamanhos){
+
+		std::vector<int> unos(L*L,1);
+
+		int percolating_count_unos = 0;
+		int total_cluster_size_unos = 0;
+
+		for (int ii = 0; ii < n_muestras; ii++){
+			ClusterInfo hk_unos = hoshen_kopelman(unos, L);
+			if (hk_unos.percolates){
+				percolating_count_unos += 1;
+				total_cluster_size_unos += hk_unos.max_cluster_size;
+			}
+		}
+		double probability_unos = static_cast<double>(percolating_count_unos) / n_muestras;
+		double mean_cluster_size_unos = static_cast<double>(total_cluster_size_unos) / (n_muestras*(L*L));
+
+		REQUIRE(probability_unos == 1.0);
+		REQUIRE(mean_cluster_size_unos==1.0);
+	}
+}
+
+TEST_CASE("Se detectan percolaciones horizontales", "[horizontal]"){
 
 	int n_muestras = 10;
 	std::vector<int> tamanhos = {10, 20, 50};
@@ -60,20 +71,10 @@ TEST_CASE("Se detectan percolaciones verticales y horizontales","[horizontal_ver
 			horizontal[l+L*fila]=1;
 		}
 
-		std::vector<int> vertical(L*L,0);
-		for(int ll=0; ll<L; ll++){
-			vertical[ll*L]= 1;
-		}
-
 		int percolating_count_hor = 0;
 		int total_cluster_size_hor = 0;
 
-		int percolating_count_ver = 0;
-		int total_cluster_size_ver = 0;
-
 		printmatrix(horizontal, L);
-		std::cout << "\n";
-		printmatrix(vertical,L);
 		std::cout << "\n";
 
 		for (int ii = 0; ii < n_muestras; ii++){
@@ -82,22 +83,44 @@ TEST_CASE("Se detectan percolaciones verticales y horizontales","[horizontal_ver
 				percolating_count_hor += 1;
 				total_cluster_size_hor += hk_horizontal.max_cluster_size;
 			}
+		}
+		double probability_hor = static_cast<double>(percolating_count_hor) / n_muestras;
+		double mean_cluster_size_hor = static_cast<double>(total_cluster_size_hor) / (n_muestras*(L*L));
+
+		REQUIRE(probability_hor == 1.0);
+		REQUIRE(mean_cluster_size_hor==(1.0/L));
+	}
+}
+
+TEST_CASE("Se detectan percolaciones verticales", "[vertical]"){
+
+	int n_muestras = 10;
+	std::vector<int> tamanhos = {10, 20, 50};
+
+	for (int L: tamanhos){
+
+		std::vector<int> vertical(L*L,0);
+		for(int ll=0; ll<L; ll++){
+			vertical[ll*L]= 1;
+		}
+
+		int percolating_count_ver = 0;
+		int total_cluster_size_ver = 0;
+
+		printmatrix(vertical,L);
+		std::cout << "\n";
+
+		for (int ii = 0; ii < n_muestras; ii++){
 			ClusterInfo hk_vertical = hoshen_kopelman(vertical, L);
 			if (hk_vertical.percolates){
 				percolating_count_ver += 1;
 				total_cluster_size_ver += hk_vertical.max_cluster_size;
 			}
 		}
-		double probability_hor = static_cast<double>(percolating_count_hor) / n_muestras;
 		double probability_ver = static_cast<double>(percolating_count_ver) / n_muestras;
-
-		double mean_cluster_size_hor = static_cast<double>(total_cluster_size_hor) / (n_muestras*(L*L));
 		double mean_cluster_size_ver = static_cast<double>(total_cluster_size_ver) / (n_muestras*(L*L));
 
-		REQUIRE(probability_hor == 1.0);
-		REQUIRE(mean_cluster_size_hor==(1.0/L));
 		REQUIRE(probability_ver == 1.0);
 		REQUIRE(mean_cluster_size_ver==(1.0/L));
-		
 	}
 }
